92b50363f182d59ddb8f5e196771b709f5e93707
Heisenburn <marcinrudnik1996@gmail.com>
2020-02-04 15:22:32 +0100

    force svg load first

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,34 @@
 
 
 //increasing height of inner vertical line
+
+$(document).ready(function(){
+  $("#empyreanSVG").load("header.html");
+   
+
+
+ });
  
-$(document).ready(function() {
+ var height = oheight = $("#verticalLine").height();
+  $(document).scroll(function () {
+    height = $(this).scrollTop() / ($(document).height() - $(window).height()) * oheight;
+    $("#progressBar").css({
+      'height': height,
 
-
-
- 
-
-    var height = oheight = $("#verticalLine").height();
-    $(document).scroll(function () {
-      height = $(this).scrollTop() / ($(document).height() - $(window).height()) * oheight;
-      $("#progressBar").css({
-        'height': height,
-
-      })
-    });
-
-
+    })
   });
+ 
+
+ 
+
+ 
+
+ 
+   
+
+ 
+
+  
+
+
+  
