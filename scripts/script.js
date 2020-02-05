ab4bb76852b3abc8370b7164db7571c052caf393
Heisenburn <marcinrudnik1996@gmail.com>
2020-02-05 11:18:16 +0100

    added scrollTo -100

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
 
+$( document ).ready(function() {
+   
+
+  window.scrollTo(0, -100);
+
 
 //increasing height of inner vertical line
- 
+
+
 if(screen.width >= 768){
  
- var height = oheight = $("#verticalLine").height();
-  $(document).scroll(function () {
-    height = $(this).scrollTop() / ($(document).height() - $(window).height()) * oheight;
-    $("#progressBar").css({
-      'height': height,
-
-    })
-  });
+  var height = oheight = $("#verticalLine").height();
+   $(document).scroll(function () {
+     height = $(this).scrollTop() / ($(document).height() - $(window).height()) * oheight;
+     $("#progressBar").css({
+       'height': height,
  
+     })
+   });
+  
+  }
 
-}
+});
+
+
+
+
 
  
 
